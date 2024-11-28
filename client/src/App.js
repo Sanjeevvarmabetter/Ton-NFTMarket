ccd0b4816c3b84c49ad5da14ae12f9dcdc6f2499
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-11-28 15:14:39 +0530

    added wallet connection

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { TonConnectUIProvider, TonConnectButton } from "@tonconnect/ui-react";
 
 function App() {
   return (
-    <TonConnectUIProvider manifestUrl="/manifest.json">
+    <TonConnectUIProvider manifestUrl="https://ton-nft-market.vercel.app/manifest.json">
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "50px" }}>
         {/* Navbar */}
         <nav style={{ width: "100%", padding: "10px 20px", backgroundColor: "#282c34", color: "white" }}>
